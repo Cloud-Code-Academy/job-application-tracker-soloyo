68f7224d779fc0e4f8463811883d79291d60d9ca
terrence.gonsalves <terrence.gonsalves@gmail.com>
2025-04-17 18:10:46 -0600

    completed form and updated dashboard to list recent applications

--- force-app/main/default/lwc/dashboard/dashboard.js
+++ force-app/main/default/lwc/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import { LightningElement, wire, track } from 'lwc';
 import getApplicationStats from '@salesforce/apex/DashboardController.getApplicationStats';
 import getUpcomingInterviews from '@salesforce/apex/DashboardController.getUpcomingInterviews';
+import getRecentApplications from '@salesforce/apex/DashboardController.getRecentApplications';
 import getPendingTasks from '@salesforce/apex/DashboardController.getPendingTasks';
 import completedTask from '@salesforce/apex/DashboardController.completedTask';
 import { refreshApex } from '@salesforce/apex';
@@ -11,26 +12,49 @@ export default class Dashboard extends LightningElement {
     @track interviewCount = 0;
     @track offerCount = 0;
     @track totalCount = 0;
+    @track recentApplications = [];
     @track upcomingInterviews = [];
     @track pendingTasks = [];
 
     @wire(getApplicationStats)
-    wiredStats({ error, data }) {
+    wiredApplicationStats({ error, data }) {
         if (data) {
-            this.appliedCount = data.appliedCount || this.appliedCount;
-            this.interviewCount = data.interviewCount || this.interviewCount;
-            this.offerCount = data.offerCount || this.offerCount;
-            this.totalCount = data.totalCount || this.totalCount;
-            this.upcomingInterviews = data.upcomingInterviews || this.upcomingInterviews;
-            this.pendingTasks = data.pendingTasks || this.pendingTasks;
+            this.appliedCount = data.applied || this.appliedCount;
+            this.interviewCount = data.interviews || this.interviewCount;
+            this.offerCount = data.offers || this.offerCount;
+            this.totalCount = data.total || this.totalCount;
         } else if (error) {
             console.error('Error fetching application stats:', error);
         }
     }
 
+    @wire(getRecentApplications)
+    wiredRecentApplications({ error, data }) {
+        if (data) {
+            const options = {
+                weekday: "long",
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              };
+
+            this.recentApplications = data.map(application => {
+                const appliedDate = new Date(application.Application_Date__c);
+                const applicationSalary = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(application.Salary__c);
+
+                return {
+                    ...application,
+                    formattedDate: appliedDate.toLocaleDateString("en-GB", options),
+                    formattedSalary: applicationSalary
+                };
+            });
+        } else if (error) {
+            console.error('Error fetching recent applications:', error);
+        }
+    }
+
     @wire(getUpcomingInterviews)
     wiredInterviews({ error, data }) {
-        this.interviewResults = data;
         if (data) {
             this.upcomingInterviews = data.map(interview => {
                 const startDate = new Date(interview.startDate);
@@ -48,7 +72,6 @@ export default class Dashboard extends LightningElement {
 
     @wire(getPendingTasks)
     wiredTasks({ error, data }) {
-        this.tasksResults = data;
         if (data) {
             this.pendingTasks = data.map(task => {
                 const dueDate = task.ActivityDate ? new Date(task.ActivityDate) : null;
--- force-app/main/default/lwc/newApplications/newApplications.js
+++ force-app/main/default/lwc/newApplications/newApplications.js
@@ -1,16 +1,23 @@
-import { LightningElement } from 'lwc';
-//import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { LightningElement, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class NewApplications extends LightningElement {
+    @track showSpinner = false;
+
     handleSubmit(e) {
         e.preventDefault();
 
+        this.showSpinner = true;
+
         const fields = e.detail.fields;
+
         const currentDate = new Date();
         const currentDateFollowUp = currentDate.setDate(currentDate.getDate() + 2);
 
+        console.log("Current Date Follow Up:", currentDateFollowUp);
+
         const formattedDate = currentDate.toISOString().split('T')[0]; 
-        const formattedFollowUpDate = currentDateFollowUp.toISOString().split('T')[0];
+        //const formattedFollowUpDate = currentDateFollowUp.toISOString().split('T')[0];
         
 
         // set some defaults just in case
@@ -22,11 +29,28 @@ export default class NewApplications extends LightningElement {
             fields.Application_Date__c = formattedDate;
         }
 
-        if (!fields.Follow_up_Date__c) {
-            fields.Follow_up_Date__c = formattedFollowUpDate;
-        }
+        // if (!fields.Follow_up_Date__c) {
+        //     fields.Follow_up_Date__c = formattedFollowUpDate;
+        // }
+
+        console.log('Fields:', fields);
         
         this.template.querySelector('lightning-record-edit-form').submit(fields);
+    }
+
+    handleSuccess() {
+        this.showSpinner = false;
+        
+        const toastEvent = new ShowToastEvent({
+            title: 'Success',
+            message: 'Application created successfully!',
+            variant: 'success'
+        });
+
+        this.dispatchEvent(toastEvent);
+
+        // reset the form after successful submission
+        this.handleCancel();
     }
 
     handleCancel() { 
